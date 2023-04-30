55746d3f1186134dd8231f0dd5fd7bfec566652b
Louis Dyer <louisalexanderdyer@gmail.com>
2023-04-30 15:24:50 +1200

    Added animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,91 @@
 import { Courier_Prime } from 'next/font/google'
+import { useEffect, useState } from 'react'
 
 const courier = Courier_Prime({
   subsets: ['latin'],
   weight: '400',
-  // style: 'normal',
-})
-
-const courierBold = Courier_Prime({
-  subsets: ['latin'],
-  weight: '700',
-  // style: 'normal',
 })
 
 export default function Home() {
-  console.log(courier)
+  const [color, setColor] = useState('text-white')
+
+  useEffect(() => {}, [color])
+
+  const hover = () => {
+    setColor('text-gray-800')
+  }
+
+  const unhover = () => {
+    setColor('text-white')
+  }
+
   return (
-    <main className={`${courier.className} 'Courier New', monospace`}>
-      {/* <div className="h-max text-9xl flex min-h-screen flex-col content-center items-center justify-between p-24">
-        <div className="h-max flex flex-row content-center items-center"> */}
-      {/* <div className="grid grid-cols-10 place-content-center text-9xl h-screen"> */}
-
-      {/* justify-between	justify-content: space-between;
-      justify-around	justify-content: space-around;
-      justify-evenly	justify-content: space-evenly;
-      justify-stretch */}
-
+    <main
+      className={`${courier.className}  transition duration-500 ${color} bg-black`}
+    >
       <div className="flex w-screen h-screen justify-center items-center uppercase">
         <div className="text-3xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl uppercase">
-          <div className="flex justify-between">
-            <p>Louis&nbsp;</p>
-            <p>Dyer&nbsp;</p>
+          <div className="flex justify-between transition">
+            <p>Louis</p>
+            <p>Dyer</p>
             <p>Full</p>
           </div>
-          <div className="flex justify-between">
-            <p>Stack&nbsp;</p>
+          <div className="flex justify-between transition">
+            <p>Stack</p>
             <p>Developer</p>
           </div>
           <div>
             <a
-              className="flex justify-between"
+              onMouseEnter={hover}
+              onMouseOut={unhover}
+              className="underline sm:no-underline flex justify-between transition hover:text-white hover:duration-500 hover:scale-110"
+              // className="flex justify-between transition hover:rotate-6 hover:text-white hover:-translate-y-10 hover:-translate-x-10 hover:duration-500 hover:scale-110"
               href="tel:+64272545605"
               target="_blank"
             >
-              <p>+64&nbsp;</p>
-              <p>27&nbsp;</p>
-              <p>254&nbsp;</p>
+              <p>+64</p>
+              <p>27</p>
+              <p>254</p>
               <p>5605</p>
             </a>
           </div>
           <div className="flex justify-between">
-            <a href="mailto:louisalexanderdyer@gmail.com" target="_blank">
-              <p>Email&nbsp;</p>
+            <a
+              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110"
+              onMouseEnter={hover}
+              onMouseOut={unhover}
+              href="mailto:louisalexanderdyer@gmail.com"
+              target="_blank"
+            >
+              <p>Email</p>
             </a>
-            <a href="https://www.linkedin.com/in/louisdyer/" target="_blank">
+            <a
+              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110"
+              onMouseEnter={hover}
+              onMouseOut={unhover}
+              href="https://www.linkedin.com/in/louisdyer/"
+              target="_blank"
+            >
               <p>LinkedIn</p>
             </a>
           </div>
           <div className="flex justify-between">
-            <a href="https://github.com/doilyuser" target="_blank">
-              <p>Github&nbsp;</p>
+            <a
+              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110"
+              onMouseEnter={hover}
+              onMouseOut={unhover}
+              href="https://github.com/doilyuser"
+              target="_blank"
+            >
+              <p>Github</p>
             </a>
-            <a href="/portfolio">
+            <p>&nbsp;</p>
+            <a
+              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110"
+              onMouseEnter={hover}
+              onMouseOut={unhover}
+              href="/portfolio"
+            >
               <p>Portfolio</p>
             </a>
           </div>
