988720d0df9a671bb5e5f5459815d3e3c5049f78
Louis Dyer <louisalexanderdyer@gmail.com>
2023-05-09 15:54:13 +1200

    installed scroll animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Courier_Prime } from 'next/font/google'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const courier = Courier_Prime({
   subsets: ['latin'],
@@ -21,86 +22,93 @@ export default function Home() {
   }
 
   return (
-    <main
-      className={`${courier.className} transition duration-500 ${color} bg-black`}
-    >
-      <div className="flex w-screen h-screen justify-center items-center uppercase">
-        <div className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl uppercase">
-          <div className="flex justify-between transition">
-            <p>Louis</p>
-            <p>Dyer</p>
-            <p>Full</p>
-          </div>
-          <div className="flex justify-between transition">
-            <p>Stack</p>
-            <p>Developer</p>
-          </div>
-          <div>
-            <a
-              onMouseEnter={hover}
-              onMouseOut={unhover}
-              className="underline sm:no-underline flex justify-between transition hover:text-white hover:duration-500 hover:scale-110"
-              // className="flex justify-between transition hover:rotate-6 hover:text-white hover:-translate-y-10 hover:-translate-x-10 hover:duration-500 hover:scale-110 hover:blur-none"
-              href="tel:+64272545605"
-              target="_blank"
-            >
-              <p>+64</p>
-              <p>27</p>
-              <p>254</p>
-              <p>5605</p>
-            </a>
-          </div>
-          <div className="flex justify-between">
-          <a
-              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
-              onMouseEnter={hover}
-              onMouseOut={unhover}
-              href="mailto:louisalexanderdyer@gmail.com"
-              target="_blank"
-            >
-              <p>Email</p>
-            </a>
-            <a
-              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
-              onMouseEnter={hover}
-              onMouseOut={unhover}
-              href="/Louis-CV.pdf"
-              download
-            >
-              <p>CV</p>
-            </a>
-            <a
-              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
-              onMouseEnter={hover}
-              onMouseOut={unhover}
-              href="https://www.linkedin.com/in/louisdyer/"
-              target="_blank"
-            >
-              <p>LinkedIn</p>
-            </a>
-          </div>
-          <div className="flex justify-between">
-            <a
-              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
-              onMouseEnter={hover}
-              onMouseOut={unhover}
-              href="https://github.com/doilyuser"
-              target="_blank"
-            >
-              <p>Github</p>
-            </a>
-            <p>&nbsp;</p>
-            <Link
-              className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
-              onMouseEnter={hover}
-              onMouseOut={unhover}
-              href="/portfolio"
-            >
-              <p>Portfolio</p>
-            </Link>
+    <>
+      <Head>
+        <title>Louis&apos; Portfolio</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main
+        className={`${courier.className} transition duration-500 ${color} bg-black`}
+      >
+        <div className="flex w-screen h-screen justify-center items-center uppercase">
+          <div className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl uppercase">
+            <div className="flex justify-between transition">
+              <p>Louis</p>
+              <p>Dyer</p>
+              <p>Full</p>
+            </div>
+            <div className="flex justify-between transition">
+              <p>Stack</p>
+              <p>Developer</p>
+            </div>
+            <div>
+              <a
+                onMouseEnter={hover}
+                onMouseOut={unhover}
+                className="underline sm:no-underline flex justify-between transition hover:text-white hover:duration-500 hover:scale-110"
+                // className="flex justify-between transition hover:rotate-6 hover:text-white hover:-translate-y-10 hover:-translate-x-10 hover:duration-500 hover:scale-110 hover:blur-none"
+                href="tel:+64272545605"
+                target="_blank"
+              >
+                <p>+64</p>
+                <p>27</p>
+                <p>254</p>
+                <p>5605</p>
+              </a>
+            </div>
+            <div className="flex justify-between">
+              <a
+                className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
+                onMouseEnter={hover}
+                onMouseOut={unhover}
+                href="mailto:louisalexanderdyer@gmail.com"
+                target="_blank"
+              >
+                <p>Email</p>
+              </a>
+              <a
+                className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
+                onMouseEnter={hover}
+                onMouseOut={unhover}
+                href="/Louis-CV.pdf"
+                download
+              >
+                <p>CV</p>
+              </a>
+              <a
+                className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
+                onMouseEnter={hover}
+                onMouseOut={unhover}
+                href="https://www.linkedin.com/in/louisdyer/"
+                target="_blank"
+              >
+                <p>LinkedIn</p>
+              </a>
+            </div>
+            <div className="flex justify-between">
+              <a
+                className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
+                onMouseEnter={hover}
+                onMouseOut={unhover}
+                href="https://github.com/doilyuser"
+                target="_blank"
+              >
+                <p>Github</p>
+              </a>
+              <p>&nbsp;</p>
+              <Link
+                className="underline sm:no-underline transition hover:text-white hover:duration-500 hover:scale-110 hover:blur-none"
+                onMouseEnter={hover}
+                onMouseOut={unhover}
+                href="/portfolio"
+              >
+                <p>Portfolio</p>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,11 +1,14 @@
 import { Courier_Prime } from 'next/font/google'
 import { useEffect, useState } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import Nav from './Components/Nav'
-import PortfolioSplash from './Components/PortfolioSplash';
-import PortfolioArticle from './Components/PortfolioArticle';
-import PingpongArticle from './Components/PingpongArticle';
-import MoneyArticle from './Components/MoneyArticle';
-import OlderProjects from './Components/OlderProjects';
+import PortfolioSplash from './Components/PortfolioSplash'
+import PortfolioArticle from './Components/PortfolioArticle'
+import PingpongArticle from './Components/PingpongArticle'
+import MoneyArticle from './Components/MoneyArticle'
+import OlderProjects from './Components/OlderProjects'
+import Head from 'next/head'
 
 const courier = Courier_Prime({
   subsets: ['latin'],
@@ -15,7 +18,12 @@ const courier = Courier_Prime({
 export default function Portfolio() {
   const [color, setColor] = useState('text-white')
 
-  useEffect(() => {}, [color])
+  useEffect(() => {
+    AOS.init()
+  }, [color])
+  // useEffect(() => {
+  //   AOS.init()
+  // }, [])
 
   const hover = () => {
     setColor('text-gray-800')
@@ -26,17 +34,31 @@ export default function Portfolio() {
   }
 
   return (
-    <div
-      className={`${courier.className} transition duration-500 ${color} bg-black`}
-    >
-      <Nav />
-      <div className="flex flex-col snap-y snap-mandatory h-screen overflow-x-hidden">
-        <PortfolioSplash />
-        <PortfolioArticle />
-        <PingpongArticle />
-        <MoneyArticle />
-        <OlderProjects />
+    <>
+      <Head>
+        <title>Louis&apos; Portfolio</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div
+        className={`${courier.className} transition duration-500 ${color} bg-black`}
+      >
+        <Nav />
+        {/* First object needs padding
+        <div className="pt-[50vh]"></div>
+        Then we need to add animations (maybe change the padding to vh)
+        <div
+          data-aos="fade-up"
+          className="px-32 my-32 text-base pt-6 pb-6 overflow-y-scroll scrollbar-hide"
+        ></div> */}
+        <div className="flex flex-col snap-y snap-mandatory h-screen overflow-x-hidden">
+          <PortfolioSplash />
+          <PortfolioArticle />
+          <PingpongArticle />
+          <MoneyArticle />
+          <OlderProjects />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
