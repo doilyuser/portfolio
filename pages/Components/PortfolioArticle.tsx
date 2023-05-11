61c7382453290d4eaa643b9b5175ec55a59c94d6
Louis Dyer <louisalexanderdyer@gmail.com>
2023-05-11 17:04:28 +1200

    add padding

--- pages/Components/PortfolioArticle.tsx
+++ pages/Components/PortfolioArticle.tsx
@@ -11,14 +11,19 @@ const courierBold = Courier_Prime({
 
 export default function PortfolioArticle() {
   return (
-    <div className="grid justify-items-start w-8/12">
-      <h1 className="pt-[50vw] text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl uppercase">
-        Portfolio
-      </h1>
-      <p className="text-2xl">
-        <span className={`${courierBold.className}`}>28.04.23</span>
-      </p>
-      <p className="text-base pt-3 pb-6 overflow-y-scroll scrollbar-hide">
+    <div className="w-full flex flex-col justify-center place-items-center md:grid md:justify-items-start md:w-4/5 lg:w-3/5 2xl:w-2/5">
+      {/* <div className="grid pl-[30vw] justify-items-end"> */}
+      <div>
+        <h1 className="pt-96 text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl xl:pt-60 uppercase">
+          Portfolio
+        </h1>
+        <p
+          className={`${courierBold.className} text-2xl md:textl-3xl lg:text-4xl pb-20`}
+        >
+          28.04.23
+        </p>
+      </div>
+      <p className="text-base py-20">
         <span className={`${courierBold.className}`}>Description:</span> My
         final task at Dev Academy was to create and deploy this website. A place
         to showcase my work as I turn my eye to employment. I took this as an
@@ -26,13 +31,13 @@ export default function PortfolioArticle() {
         Next.js & deploying with Vercel. Isn&apos;t that what all the cool kids
         are doing these days?
       </p>
-      <div className="flex justify-center place-items-center overflow-x-scroll scrollbar-hide">
+      <div className="flex py-20">
         <React />
         <Tailwind />
         <Next />
         <Vercel />
       </div>
-      <p className="text-base pt-6 pb-6 overflow-y-scroll scrollbar-hide">
+      <p className="text-base py-20">
         <span className={`${courierBold.className}`}>Reflections:</span> Coding
         without the structure of Dev Academy around is a lot harder. Both to
         maintain a schedule and focus on the MVP. More than a few times I got
@@ -43,7 +48,7 @@ export default function PortfolioArticle() {
         feel I&apos;ve made any inroads with Next.js, perhaps that should be my
         &quot;next&quot; adventure.
       </p>
-      <div className="underline hover:no-underline">
+      <div className="underline hover:no-underline py-20">
         <a className="" href="https://github.com/doilyuser/portfolio">
           <p>Github</p>
         </a>
