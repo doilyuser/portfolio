df7d2fd8030ce57b6511564d8b54f97521e05509
Louis Dyer <louisalexanderdyer@gmail.com>
2023-05-12 12:49:56 +1200

    offset every second article

--- pages/Components/Pingpong.tsx
+++ pages/Components/Pingpong.tsx
@@ -29,7 +29,7 @@ export default function PortfolioArticle() {
     setColor('text-white')
   }
   return (
-    <div className="w-full flex flex-col justify-center place-items-center md:grid md:justify-items-start md:w-10/12 lg:w-8/12">
+    <div className="w-full flex flex-col justify-center place-items-center md:grid md:justify-items-end md:pl-[16.66%] lg:pl-[33.33%]">
       {/* <div className="grid pl-[30vw] justify-items-end"> */}
       <div data-aos="fade-left" data-aos-offset="300">
         <h1 className="pt-40 text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl xl:pt-60 uppercase">
